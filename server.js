f7c0723f8613c763ccb7e7805d57f7975d0b1c0b
VijaiNatesh <glennvijay@gmail.com>
2022-02-21 08:56:20 +0500

    sever

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.use("/api/order", orderRoute)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on ${PORT}`)
 })
